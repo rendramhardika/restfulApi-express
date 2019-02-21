083eef766228ace651808bb924caa66095aa8ee2
Rendra Mahardika <rendraonline5@gmail.com>
2019-02-21 10:06:44 +0700

    add fitur get data mahasiswa by nim

--- controller.js
+++ controller.js
@@ -3,6 +3,12 @@
 var response = require('./res')
 var connection = require('./conn')
 
+//routes for index
+exports.index = function(req, res){
+    response.ok("Ini halaman index, restful api using express js", res)
+}
+
+//get all data mahasiswa
 exports.mahasiswa = function(req, res){
     connection.query('SELECT * FROM mahasiswa', function(error, rows, fields){
         if(error){
@@ -14,6 +20,29 @@ exports.mahasiswa = function(req, res){
     })
 }
 
-exports.index = function(req, res){
-    response.ok("Ini halaman index, restful api using express js", res)
+//get data mahasiswa by NIM
+exports.getMahasiswa = function(req, res){
+    var nim = req.params.nim
+
+    connection.query('SELECT * FROM mahasiswa WHERE nim = ?',[nim], function(error, rows, fields){
+        if(error){
+            console.log(error)
+        }
+        else{
+            response.ok(rows, res)
+        }
+    })
+}
+
+exports.deleteMahasiswa = function(req, res){
+    var nim = req.params.nim
+
+    connection.query('DELETE FROM mahasiswa WHERE nim = ?', [nim], function(error, rows, fields){
+        if(error){
+            console.log(error)
+        }
+        else{
+            response.ok("berhasil menghapus data mahasiswa", res)
+        }
+    })
 }
--- routes.js
+++ routes.js
@@ -6,4 +6,6 @@ module.exports = function(app){
     app.route('/').get(todoList.index)
 
     app.route('/mahasiswa').get(todoList.mahasiswa)
+    app.route('/mahasiswa/:nim').get(todoList.getMahasiswa)
+    app.route('/mahasiswa/:nim').delete(todoList.deleteMahasiswa)
 }
